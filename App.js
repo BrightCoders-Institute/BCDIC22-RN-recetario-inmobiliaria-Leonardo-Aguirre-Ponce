c7b522ad9d6a6abd4779794c1fc2cac3cfda17b0
Leonardo-Aguirre-Ponce <laguirre4@ucol.mx>
2023-01-04 16:13:15 -0600

    Inicio de entorno

---
+++ App.js
@@ -0,0 +1,13 @@
+// Node modules
+import React, { Component } from 'react';
+import Navigation from './src/navigation/Index';
+
+export default class App extends Component {
+	constructor() {
+		super(props);
+	}
+
+	render() {
+		return <Navigation />;
+	}
+}
